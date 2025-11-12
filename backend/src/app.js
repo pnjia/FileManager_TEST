967b9d56254422da58be11118a0174a6339a722a
pnjia <panjiangkasaputra67@gmail.com>
2025-11-12 14:41:10 +0700

    Menambahkan cors agar dapat terbaca pada frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,18 @@ import fileRoutes from "./routes/fileRoutes.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
